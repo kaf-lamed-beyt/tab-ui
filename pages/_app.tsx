260580f68ba030832a0d020c511671693bd80fa3
kaf-lamed-beyt <belac335@gmail.com>
2023-06-13 12:26:48 +0100

    fix: head tag duplicate

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,14 +6,12 @@ import MetaData from "../components/metadata";
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <MetaData
-          url="reaact-tab.netlify.app"
-          pageTitle="react-tab"
-          description="state-aware tab components in react"
-          previewImage="https://reaact-tab.netlify.app/preview.png"
-        />
-      </Head>
+      <MetaData
+        url="reaact-tab.netlify.app"
+        pageTitle="react-tab"
+        description="state-aware tab components in react"
+        previewImage="https://reaact-tab.netlify.app/preview.png"
+      />
       <main className={roboto.className}>
         <Component {...pageProps} />
       </main>
